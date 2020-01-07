234fb048c64d5d87cc5e62e3c71d3a3e48fd514f
Steve Smith <ssmith@atlassian.com>
2020-01-07 12:56:12 +1100

    DCD-889: Add tags to all templates and update script to iterate over them.

--- scripts/update-tags.py
+++ scripts/update-tags.py
@@ -2,28 +2,35 @@
 
 import datetime
 import fileinput
+import glob
+import os
+from os.path import isfile, join
 import re
 import subprocess
 import sys
 
-TEMPLATE = 'templates/quickstart-vpc-for-atlassian-services.yaml'
-
 commit = subprocess.getoutput("git rev-parse HEAD").split()[0]
 tstamp = '{:%Y%m%d:%H.%M}'.format(datetime.datetime.utcnow())
 
-with fileinput.input(TEMPLATE, inplace=True, backup=".bak") as fd:
-    cpat = re.compile(r'^(\s+)Value:\s+"COMMIT:')
-    tpat = re.compile(r'^(\s+)Value:\s+"TIMESTAMP:')
+def update_tags(fname):
+  with fileinput.input(fname, inplace=True, backup=".bak") as fd:
+      cpat = re.compile(r'^(\s+)Value:\s+"COMMIT:')
+      tpat = re.compile(r'^(\s+)Value:\s+"TIMESTAMP:')
 
-    for line in fd:
-        cmatch = cpat.match(line)
-        tmatch = tpat.match(line)
-        if cmatch:
-            nval = cmatch.group(1)+'Value: "COMMIT: '+commit+'"\n'
-            sys.stdout.write(nval)
-        elif tmatch:
-            nval = tmatch.group(1)+'Value: "TIMESTAMP: '+tstamp+'"\n'
-            sys.stdout.write(nval)
-        else:
-            sys.stdout.write(line)
+      for line in fd:
+          cmatch = cpat.match(line)
+          tmatch = tpat.match(line)
+          if cmatch:
+              nval = cmatch.group(1)+'Value: "COMMIT: '+commit+'"\n'
+              sys.stdout.write(nval)
+          elif tmatch:
+              nval = tmatch.group(1)+'Value: "TIMESTAMP: '+tstamp+'"\n'
+              sys.stdout.write(nval)
+          else:
+              sys.stdout.write(line)
+
+
+for tmpl in glob.glob("templates/*.yaml"):
+    print("Updating "+tmpl)
+    update_tags(tmpl)
 
